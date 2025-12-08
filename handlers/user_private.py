0db697f2515375f75c38b8fe6376fcf1b93e21c4
AlexZha-dev <oleksandr.zhabchenko.work@outlook.com>
2025-12-08 20:12:21 +0200

    feat(d20 responses): aded funy responses

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 
 import common.keyboards as kb
 from states.dice_states import DiceState
+from common.d20_responses import d20_roll_messages
 
 user_private_router = Router()
 
@@ -47,22 +48,35 @@ async def make_roll(callback: types.CallbackQuery, state: FSMContext):
     funny_comments = []
 
     for r in results:
-        if sides == 20:
-            if r == 1 and rolls == 1:
-                funny_comments.append("1 💀 Фиаско!")
-            elif r == 20 and rolls == 1:
-                funny_comments.append("20 🏆 Критический успех!")
-            else:
-                funny_comments.append(f"{r}")
+        if sides == 20 and rolls == 1:
+            if r == 1:
+                comment = f"{r}\n{choice(d20_roll_messages[1])}"
+            elif 2 <= r <= 4:
+                comment = f"{r}\n{choice(d20_roll_messages[2])}"
+            elif 5 <= r <= 8:
+                comment = f"{r}\n{choice(d20_roll_messages[3])}"
+            elif 9 <= r <= 12:
+                comment = f"{r}\n{choice(d20_roll_messages[4])}"
+            elif 13 <= r <= 16:
+                comment = f"{r}\n{choice(d20_roll_messages[5])}"
+            elif 17 <= r <= 19:
+                comment = f"{r}\n{choice(d20_roll_messages[6])}"
+            elif r == 20:
+                comment = f"{r}\n{choice(d20_roll_messages[7])}"
+
+            funny_comments.append(comment)
+            results_funny = "".join(funny_comments)
         else:
             funny_comments.append(str(r))
+            results_funny = ", ".join(funny_comments)
 
-    results_funny = ", ".join(funny_comments)
+    if rolls == 1:
+        text = f"🎲 Бросок D{sides}\nРезультат броска: {results_funny}"
+    else:
+        text = f"🎲 Броски D{sides} x{rolls}\nРезультаты бросков: {results_funny}"
+        total = sum(results)
+        text += f"\nСумма: {total}"
 
-    await callback.message.edit_text(
-        f"🎲 Броски D{sides} x{rolls}\n"
-        f"Результаты: {results_funny}\n"
-        f"Сумма: {total}"
-    )
+    await callback.message.edit_text(text)
 
     await state.clear()
